7b7d206e169dcd31896d7720060f1d0d65474ccc
knakayama <knakayama.sh@gmail.com>
2014-09-22 05:41:42 +0900

    add comment

--- user-data.sh
+++ user-data.sh
@@ -1,16 +1,29 @@
 #!/bin/bash -ex
 
 {
+# change sudoer
 sed -i 's/^Defaults    requiretty/Defaults:ec2-user    !requiretty/' "/etc/sudoers"
+
+# use JST
 ln -sf "/usr/share/zoneinfo/Asia/Tokyo" "/etc/localtime"
 sed -i -e 's@"UTC"@"Asia/Tokyo"@' -e 's/true/false/' "/etc/sysconfig/clock"
+
+# change hostname
 sed -i "s/localhost\.localdomain/$HOST_NAME/" "/etc/sysconfig/network"
 hostname "$HOST_NAME"
+
+# for ohai
 mkdir -p "/etc/chef/ohai/hints"
 touch "/etc/chef/ohai/hints/ec2.json"
+
+# change ssh port
 sed -i 's/#Port 22/Port 65467/' "/etc/ssh/sshd_config"
 service sshd restart
+
+# update
 yum -y update
+
+# change mta to postfix
 service sendmail stop
 chkconfig sendmail off
 yum install -y postfix
@@ -18,5 +31,6 @@ yum install -y postfix
 yum remove -y sendmail
 service postfix start
 chkconfig postfix on
+
 } | tee -a /root/install.log
 
